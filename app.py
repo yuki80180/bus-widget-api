f8ce0e4aec538d11c01f9302ef855931e5204d6e
yuki80180 <yyamazaki081036@gmail.com>
2026-06-04 17:11:28 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 import json
 from datetime import datetime
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template  # render_templateを追加
 
 app = Flask(__name__)
+
+# トップページにアクセスしたときにWebアプリの画面を表示する
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/api/next_bus')
 def api_next_bus():
     with open('schedule.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
 
-    # iPhoneから「?dir=to_station」のように送られてきた指示を受け取る
-    # （指定がなければとりあえず「to_uni(行き)」にする）
     direction = request.args.get('dir', 'to_uni')
 
     now = datetime.now()
     current_time = now.strftime("%H:%M")
     day_type = "weekend" if now.weekday() >= 5 else "weekday"
 
-    # 指定された方向(direction)のデータを使ってリストアップ
     next_buses = [bus for bus in data[direction][day_type] if bus["time"] > current_time]
 
     if next_buses:
